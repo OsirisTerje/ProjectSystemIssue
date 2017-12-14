6406048a3ff3ebdef37ec6f2c2aead3b84e9e1e0
Terje Sandstrom <terje@hermit.no>
2017-12-14 09:16:11 +0100

    Add project files.

---
+++ ReproProjectSystem/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReproProjectSystem
+{
+    public class Class1
+    {
+    }
+}
